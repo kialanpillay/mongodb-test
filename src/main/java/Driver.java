db1a388dcf8fc7110fdf2c09363c4c3b494cf400
Kialan Pillay <kialanpillay@gmail.com>
2021-06-01 22:25:14 +0200

    Initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,34 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver {
+    public static void main(String[] args) {
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        database.drop();
+        database = mongoClient.getDatabase("mydb");
+        MongoCollection<Document> collection = database.getCollection("test");
+        Document doc = new Document("name", "MongoDB")
+                .append("type", "database")
+                .append("count", 1)
+                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
+                .append("info", new Document("x", 203).append("y", 102));
+        collection.insertOne(doc);
+        System.out.println(collection.countDocuments());
+    }
+}
